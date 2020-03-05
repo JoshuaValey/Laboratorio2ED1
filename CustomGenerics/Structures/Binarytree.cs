6bacb79b5255e5eb13c9eb1a3267852ee2c6fbcb
Marcela Estrada Rodriguez <marcelaestradarl@gmail.com>
2020-03-05 00:37:33 -0600

    Delegates and view

--- CustomGenerics/Structures/Binarytree.cs
+++ CustomGenerics/Structures/Binarytree.cs
@@ -15,6 +15,61 @@ namespace CustomGenerics.Structures
 
         #region Métodos y delegados para eliminación de nodos. 
 
+        public Node<T> createNode(T newValue, Node<T> father)
+        {
+
+            Node<T> new_Node = new Node<T>();
+            new_Node.Father = father;
+            new_Node.Value = newValue;
+            new_Node.Right = null;
+            new_Node.Left = null;
+            return new_Node;
+        }
+
+        public void insertNode(Node<T> tree, T value, Node<T> father)
+        {
+            //if the tree is empty
+            if (tree == null)
+            {
+                Node<T> new_Node = createNode(value, father);
+                tree = new_Node;
+            }
+            else //if the tree already have one or more nodes
+            {
+                T valueRoot = tree.Value; //get the root value
+                if (value.CompareTo(valueRoot) < 0) //if the element is smaller than root, insert left
+                {
+                    insertNode(tree.Left, value, tree);
+                }
+                else //if the element isn't smaller, insert right
+                {
+                    insertNode(tree.Right, value, tree);
+                }
+            }
+        }
+
+        public bool Searching(Node<T> tree, T value)
+        {
+            bool x = false;
+            if (tree == null) //tree is empty
+            {
+                x = false;
+            }
+            else if (tree.Value.CompareTo(value) == 0) //value found
+            {
+                x = true;
+            }
+            else if (value.CompareTo(tree.Value) > 0) //search on smaller elements
+            {
+                Searching(tree.Left, value);
+            }
+            else //search on bigger elements
+            {
+                Searching(tree.Right, value);
+            }
+            return x;
+        }
+
         //SobreCarga de Delete
         public void Eliminar(Node<T> actual,T value)
         {
@@ -113,75 +168,6 @@ namespace CustomGenerics.Structures
             }
         }
 
-
-        #endregion
-
-      
-
-        public void Insert(Node<T> actual, T value, Node<T> father)
-        {
-            throw new NotImplementedException();
-        }
-
-
-
-        public void Order(Node<T> node)
-        {
-
-        }
-        public Node<T> createNode(T newValue, Node<T> father)
-        {
-           
-            Node<T> new_Node = new Node<T>();
-            new_Node.Father = father;
-            new_Node.Value = newValue;
-            new_Node.Right = null;
-            new_Node.Left = null;
-            return new_Node;
-        }
-
-       /* public void insertNode(Node<T> tree, T value, Node<T> father)
-        {
-            //if the tree is empty
-            if (tree == null)
-            {
-                Node<T> new_Node = createNode(value, father);
-                tree = new_Node;
-            }
-            else //if the tree already have one or more nodes
-            {
-                T valueRoot = tree.Value; //get the root value
-                if (value < valueRoot) //if the element is smaller than root, insert left
-                {
-                    insertNode(tree.Left, value, tree);
-                }
-                else //if the element isn't smaller, insert right
-                {
-                    insertNode(tree.Right, value, tree);
-                }
-            }
-        }*/
-
-        /*public bool Searching(Node<T> tree, T value)
-        {
-            if (tree == null) //tree is empty
-            {
-                return false;
-            }
-            else if ((tree.Value).Equals(value)) //value foung
-            {
-                return true;
-            }
-            else if (value < tree.Value) //search on smaller elements
-            {
-                Searching(tree.Left, value);
-            }
-            else //search on bigger elements
-            {
-                Searching(tree.Right, value);
-            }
-        }
-        */
         public void preOrder(Node<T> tree)
         {
             if (tree == null)
@@ -210,15 +196,6 @@ namespace CustomGenerics.Structures
             }
         }
 
-        public void Search(Node<T> actual, T value)
-        {
-            throw new NotImplementedException();
-        }
-
-       
-
-
-
         public void postOrder(Node<T> tree)
         {
             if (tree == null)
@@ -231,6 +208,23 @@ namespace CustomGenerics.Structures
                 postOrder(tree.Right);
                 Console.Write(tree.Value + " - ");
             }
+        }
+        #endregion
+
+
+        public void Insert(Node<T> actual, T value, Node<T> father)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Order(Node<T> node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Search(Node<T> actual, T value)
+        {
+            throw new NotImplementedException();
         }
 
         public void Delete(Node<T> actual, T value)
