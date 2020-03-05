dd9590747afd94c47973874a2b91b9ec5436babc
JoshuaValey <j97valey@gmail.com>
2020-03-04 18:43:04 -0600

    compareTo method of Drug type

--- Models/Drug.cs
+++ Models/Drug.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace Laboratorio2ED1.Models
 {
-    public class Drug
+    public class Drug : IComparable<Drug>
     {
         public int id { get; set; }
         public string nombre { get; set; }
@@ -13,5 +13,29 @@ namespace Laboratorio2ED1.Models
         public string casa_productora { get; set; }
         public double precio { get; set; }
         public int existencia { get; set; }
+
+        /// <summary>
+        /// Comapare the name of a drug with other durg.
+        /// </summary>
+        /// <param name="comming"></param>
+        /// <returns>value betwen -1, 0 and 1.
+        /// -1 if is smaller
+        /// 0 if is equals
+        /// 1 if is bigger</returns>
+        public int CompareTo(Drug comming)
+        {
+            if (this.nombre.CompareTo(comming.nombre) == 1)
+            {
+                return 1;
+            }
+            else if (this.nombre.CompareTo(comming.nombre) == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
